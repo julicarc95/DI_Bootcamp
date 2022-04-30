096549f17a1d252043fbe2eb621e40579ed7b115
julicarc95 <101662471+julicarc95@users.noreply.github.com>
2022-04-30 12:31:06 +0300

    Add files via upload

---
+++ Day 2/ExerciseXP/ExerciseXP.py
@@ -0,0 +1,29 @@
+#Exercise 1
+a = "Hello World"
+print(a)
+print("Hello World")
+#Exercise 2
+b = 99 ** 3
+c = b * 8
+print(c)
+#Exercise 4
+computer_brand = "HP Pavillon 14- ce xxxw3r3" 
+print(f"I have a{computer_brand}computer")
+#Exercise 5
+name = "Julian"
+age = 27
+shoe_size = 10
+info = f"my name is {name}. I am {age} years old. I like shoes sized {shoe_size}"
+print(info)
+#Exercise 6
+#d = int(input("Give me a number"))
+#e = int(input("Give me another number"))
+#if d > e: 
+#    print("Hello World")
+#Exercise 7
+#g = int(input("Give me a number"))
+#if g % 2 == 0:
+#    print("Is even")
+#else:
+#    print("Is odd")
+#Exercise 8
